5f7f8192d84a4a39e7c422aa15a9e11c88839875
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-04-18 16:21:23 +0800

    Update script.js to use minified Readability.js from jsDelivr

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 // @name         chatgpt-page-translate-button
 // @description  🍓 let ChatGPT translate the web page you are reading in one click
 // @author       mefengl
-// @version      0.1.9
+// @version      0.1.10
 // @namespace    https://github.com/mefengl
-// @require      https://raw.githubusercontent.com/mozilla/readability/main/Readability.js
+// @require      https://cdn.jsdelivr.net/npm/@mozilla/readability@0.4.3/Readability.min.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=openai.com
 // @license      MIT
 // @match        *://*/*
